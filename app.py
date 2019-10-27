5419affb5626bbfb801f5f339f11f111aa472216
Aidan McCaffrey <aidan.mccaffrey@publicrelay.com>
2019-10-27 18:30:03 -0400

    updated app

--- app.py
+++ app.py
@@ -5,51 +5,37 @@ import plotly.graph_objs as go
 import pandas as pd
 import psycopg2 as pg
 
-###########  Define PostgreSQL connection
+######  Define PostgreSQL connection
 conn = pg.connect(
-    database="airbus",
-    user="tableau",
-    password="d3f@ulT_9G_p@ss",
-    host="dbgw.publicrelay.com",
+    database="postgres",
+    user="postgres",
+    password="pg_p@ss",
+    host="localhost",
     port="5432")
 
-########### Define article volume function
-def volume(tag):
-    return pd.read_sql_query(
+###### Define DataFrame by reading in data from PostgreSQL
+df = pd.read_sql_query(
     f"""
-    SELECT COUNT (DISTINCT article_id)
-    FROM bhqlite
-    WHERE intake_date >= '12/31/2015'
-    AND intake_date <= '1/1/2017'
-    AND is_primary = True
-    AND tag_name = '{tag}'
+    SELECT article_id, outlet_name, tag_name, provider, provider_value, zip_code
+    FROM airbus_bhqlite_excerpt
     """,
-    con=conn)['count'][0]
+    con=conn)
+
+###### Create parent_tag and child_tag columns from tag_name column
+df['parent_tag'] = df['tag_name'].str.split(':').str.get(0)
+df['child_tag'] = df['tag_name'].str.split(':').str.get(1)
+
+###### Remove unnecessary rows
+indexNames = df[(df['provider'] == 'Majestic') | (df['provider'] == 'GnipTwitter:Retro') | (df['provider'] == 'LinkedIn') | (df['provider'] == 'Facebook:comments')].index
+df.drop(indexNames, inplace=True)
 
 ########### Define variables
-tags=['Airbus', 'Boeing', 'Bombardier', 'Embraer']
-volumes=[volume(tag) for tag in tags]
-color='#303f9f'
-title='Article Volume by Company'
+parent_tag_list=['Airbus', 'Boeing']
+child_tag_list = ['Aircraft Production', 'Corporate', 'Financial Perf.', 'Safety', 'Government Relations', 'Technology']
+title='Aerospace Media Coverage: 2016'
 tabtitle='PublicRelay'
-myheading= 'Airbus Competitors'
-label='Volume'
-
-########### Set up the chart
-article_volume = go.Bar(
-    x=tags,
-    y=volumes,
-    name=label,
-    marker={'color':color}
-)
-
-data = article_volume
-layout = go.Layout(
-    barmode='group',
-    title = title
-)
-
-fig = go.Figure(data=data, layout=layout)
+githublink='https://github.com/aidanjdm/airbus'
+#label='Volume'
 
 
 ########### Initiate the app
@@ -60,13 +46,54 @@ app.title=tabtitle
 
 ########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1(myheading),
-    dcc.Graph(
-        id='Airbus',
-        figure=fig
-    ),
+    html.H1(title),
+    html.Div('Select Company:'),
+    dcc.Dropdown(id='parent-dropdown', options= [{'label':i, 'value':i} for i in parent_tag_list], value=parent_tag_list[0]),
+    html.Div('Select Topic:'),
+    dcc.Dropdown(id='child-dropdown', options= [{'label':i, 'value':i} for i in child_tag_list], value=child_tag_list[0]),
+    dcc.Graph(id='output-div'),
+    html.A('Code on Github', href=githublink),
     ]
 )
+
+############ Callbacks
+@app.callback(dash.dependencies.Output('output-div', 'figure'),
+                [dash.dependencies.Input('parent-dropdown', 'value'), dash.dependencies.Input('child-dropdown', 'value')])
+def getChart(parent, child):
+    ##### Define filtered DataFrame and create outlet volume and sharing DataFrame from filtered DataFrame
+    df_filtered = df[df['tag_name'] == f'{parent}:{child}']
+    df_outlet_volume = df_filtered.groupby('outlet_name')[['article_id']].nunique().reset_index()
+    df_outlet_sharing = df_filtered.groupby(['outlet_name', 'article_id', 'provider'])[['provider_value']].min().reset_index()
+    df_outlet_sharing_totals = df_outlet_sharing.groupby('outlet_name')[['provider_value']].sum().reset_index()
+    df_outlet_merged = pd.merge(df_outlet_volume, df_outlet_sharing_totals, how='left', on='outlet_name')
+    df_outlet_merged = df_outlet_merged.fillna(0)
+
+    trace = go.Scatter(
+    x = df_outlet_merged['article_id'],
+    y = df_outlet_merged['provider_value'],
+    hovertext = df_outlet_merged['outlet_name'],
+    hoverinfo = 'text+x+y',
+    mode = 'markers',
+    marker=dict(
+        size=11,
+        color = df_outlet_merged['article_id'], # set color equal to a third variable
+        colorscale="viridis",
+        reversescale=True,
+        showscale=True
+        )
+    )
+
+    z = df_outlet_merged['outlet_name']
+
+    data = [trace]
+    layout = go.Layout(
+        title = f'Outlets covering {parent} {child} by Article Volume and Social Sharing', # Graph title
+        xaxis = dict(title = 'Article Volume'), # x-axis label
+        yaxis = dict(title = 'Social Shares'), # y-axis label
+        hovermode ='closest' # handles multiple points landing on the same vertical
+        )
+    fig = go.Figure(data=data, layout=layout)
+    return fig
 
 if __name__ == '__main__':
     app.run_server()
